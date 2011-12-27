f2f32e83eb6ed851215d19a4e296b22d80eef2a7
nayanajith <nml@ucsc.lk>
2011-12-27 20:39:16 +0530

    stored proc almost complete

--- sis_module/process/test.sql
+++ sis_module/process/test.sql
@@ -8,12 +8,10 @@ BEGIN
    -- Parameter for the compulsory courses
    -- the list of comma seperated courses which student should sit
    DECLARE to_sit_comp_       TEXT;
-   -- The minimum number of course which the student should sit
-   DECLARE to_sit_comp_min_   INT(2);
-   -- The number of courses which the student have to sit
+   -- Number of all compulsory courses student have still to sit
    DECLARE to_sit_comp_count_ INT(2);
-   -- The number of courses which the student have sit
-   DECLARE comp_count_        INT(2);
+   -- Number of all compulsory courses for this year
+   DECLARE comp_all_count_    INT(2);
 
    -- Parameter for the optional courses
    -- the list of comma seperated courses which student could sit
@@ -41,12 +39,14 @@ BEGIN
    DECLARE min_gpa            FLOAT; 
    -- Students current GPA
    DECLARE gpa                FLOAT; 
+   -- to hold wether student passwd the year or not
+   DECLARE pass               BOOLEAN; 
+   -- to hold wether student eligible for HDIT
+   DECLARE HDIT               BOOLEAN; 
+
 
    -- 4 optional course should be passed ('C') to get the HDIT 
    SET to_sit_opt_min_  = 4;
-
-   -- 4(ALL) compulsory course should be passed ('C') to get the HDIT  [ OBVIOUS ]
-   SET to_sit_comp_min_ = 4;
 
    -- passing the optional lms courses is not required to get the HDIT
    SET to_sit_lms_min_  = 0;
@@ -64,11 +64,11 @@ BEGIN
    -- The list of optional course not sit and the count of the student
    SELECT GROUP_CONCAT(course_id),COUNT(*) INTO to_sit_opt_,to_sit_opt_count_ FROM bit_course WHERE revision=revision_ AND student_year=year_ AND NOT compulsory AND LEFT(alt_course_id,4) NOT IN( SELECT LEFT(alt_course_id,4) FROM bit_marks INNER JOIN bit_course USING(course_id) WHERE index_no=index_no_ AND student_year=year_ AND repeat_max >= 1 AND NOT compulsory AND grand_final_mark >= 50);
 
-   -- count of compulsory courses sit by the student
-   SELECT count(*) INTO opt_count_  FROM bit_marks INNER JOIN bit_course USING(course_id) WHERE index_no=index_no_ AND student_year=year_ AND repeat_max >= 1 AND NOT compulsory AND grand_final_mark >= 50;
-
    -- count of optional courses sit by the student
-   SELECT count(*) INTO comp_count_ FROM bit_marks INNER JOIN bit_course USING(course_id) WHERE index_no=index_no_ AND student_year=year_ AND repeat_max >= 1 AND compulsory AND grand_final_mark >= 50;
+   SELECT count(*) INTO opt_count_ FROM bit_marks INNER JOIN bit_course USING(course_id) WHERE index_no=index_no_ AND student_year=year_ AND repeat_max >= 1 AND NOT compulsory AND grand_final_mark >= 50;
+
+   -- count of all compulsory courses for the year
+   SELECT count(*) INTO comp_all_count_ FROM bit_course WHERE student_year=year_ AND revision=revision_  AND compulsory;
 
    -- year-3 total gpa of the student 
    SELECT class_gpa INTO gpa FROM bit_gpa WHERE index_no=index_no_ AND year='3T';
@@ -84,46 +84,52 @@ BEGIN
    -- the last date of the last exam which sit by the student to pass the DEPLOMA/HDEPLOMA/DEGREE
    SELECT MAX(LEFT(exam_hid,10)) INTO effective_date FROM bit_marks INNER JOIN bit_course USING(course_id) WHERE index_no=index_no_ AND student_year=year_ AND repeat_max >= 1;
 
-   -- RETURN CONCAT(lms_count_,opt_count_,comp_count_);
-
-   IF lms_count_ >= to_sit_lms_min_ AND  opt_count_ >= to_sit_opt_min_ AND comp_count_ >= to_sit_comp_min_ THEN
-      -- change the students stat into pass if the condition are fulfilled
-      REPLACE INTO bit_student_state(`index_no`,`year2_state`,`year2_date`)values(index_no_,'HDIT',effective_date);
-      RETURN CONCAT_WS(',','HDIT',effective_date);
+   -- If the student satisfy all the conditions make an entry for HDIT
+   IF lms_count_ >= to_sit_lms_min_ AND  opt_count_ >= to_sit_opt_min_ AND to_sit_comp_count_ >= comp_all_count_ THEN
+      REPLACE INTO bit_student_state(`index_no`,`year2_state`,`year2_pass`,`year2_date`)values(index_no_,'HDIT',pass,effective_date);
    ELSE
+      -- Prepare the comment for the compulsory courses
+      IF to_sit_comp_count_ = 0 THEN
+         SET to_sit_comp_  = '';
+      ELSE
+         IF to_sit_comp_count_ = comp_all_count_ THEN
+            SET to_sit_comp_  = 'TO SIT: ALL Compulsory';
+         ELSE
+            SET to_sit_comp_  = CONCAT('TO SIT: ',to_sit_comp_);
+         END IF;
+      END IF;
+
+      -- Prepare the comment for the optional courses
+      IF to_sit_opt_count_ = 0 THEN
+         SET to_sit_opt_  = '';
+      ELSE
+         SET to_sit_opt_  = CONCAT('TO SIT:',to_sit_opt_min_-opt_count_,' FROM (',to_sit_opt_,')');
+      END IF;
+
+
+      -- Prepare the comment for the lms courses
+      IF lms_count_ >= to_sit_lms_min_ THEN
+         SET to_sit_lms_ = '';
+      ELSE
+         SET to_sit_lms_ = 'TO SIT: LMS Assesments';
+      END IF;
+
+
+      -- Checking the pass criteria of year-2
       IF gpa >= min_gpa THEN
-         -- If the student aquire the minimum GPA student will passw year 2
-         REPLACE INTO bit_student_state(`index_no`,`year2_state`,`year2_date`)values(index_no_,'HDIT',effective_date);
-      END IF;
-
-
-      SET to_sit_comp_  = CONCAT('TO SIT:',to_sit_comp_);
-
-      IF !lms_count_ AND year_ = 1 THEN
-         SET to_sit_lms_ = ' TO SIT: LMS Assesments';
+         SET pass=TRUE;
       ELSE
-         SET to_sit_lms_ = ' ';
+         SET pass=false;
       END IF;
 
-      IF comp_count_ = 0 THEN
-         SET to_sit_comp_ = CONCAT('TO SIT: ALL Compulsory');
-      ELSE
-         SET to_sit_comp_ = CONCAT('TO SIT:',to_sit_comp_);
-      END IF;
 
-      IF opt_count_ = 0 THEN
-         SET to_sit_opt_  = CONCAT(' TO SIT: ',to_sit_opt_min_,' FROM All Optional courses');
-      END IF;
-
-      IF opt_count_ != 0 AND to_sit_opt_min_ > opt_count_ THEN
-         SET to_sit_opt_  = CONCAT(' TO SIT:',to_sit_opt_min_-opt_count_,' FROM (',to_sit_opt_,')');
-      ELSE
-         SET to_sit_opt_  = ' ';
-      END IF;
-      
-      REPLACE INTO bit_student_state(`index_no`,`year2_state`)values(index_no_,CONCAT_WS(to_sit_comp_,to_sit_opt_,to_sit_lms_));
-      RETURN CONCAT_WS(',',to_sit_comp_,to_sit_opt_,to_sit_lms_);
+      -- update the database with the information found
+      REPLACE INTO bit_student_state(`index_no`,`year2_state`,year2_pass)values(index_no_,CONCAT_WS(to_sit_comp_,to_sit_opt_,to_sit_lms_),pass);
    END IF;
+   
+   -- return some information to the query 
+   RETURN CONCAT_WS(', ',to_sit_comp_,to_sit_opt_,to_sit_lms_,pass,'HDIT:',HDIT,lms_count_,opt_count_,to_sit_comp_count_);
+
 END;             
 ||
 delimiter ;
@@ -135,9 +141,7 @@ FROM  bit_marks AS m
 INNER JOIN bit_course AS c
 USING(course_id)
 WHERE c.student_year=2
-AND m.index_no='0104523'
 GROUP BY index_no
-LIMIT 2000
 
 /*
 REPLACE INTO bit_student_state()(m.index_no,

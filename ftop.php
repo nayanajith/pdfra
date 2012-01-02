f9ea241a0f8415965778d4343ecbc5c0e5825f98
nayanajith <nml@ucsc.lk>
2012-01-02 21:14:17 +0530

    aa

---
+++ ftop.php
@@ -0,0 +1,33 @@
+<?php
+//============================================================+
+// File name   : ftop.php
+// Version     : 1.0.008
+// Begin       : 2011-04-09
+// Last Update : 2012-12-16
+// Author      : Nayanajith mahendra laxaman - UCSC - 35. Reid avenue, Colombo 7 - Sri Lanka - www.ucsc.lk - nml@ucsc.lk
+// License     : GNU-LGPL v3 (http://www.gnu.org/copyleft/lesser.html)
+// -------------------------------------------------------------------
+// Copyright (C) 2011-2012  Nayanajith mahendra Laxaman . UCSC
+//
+// This file is part of PMDOE software library.
+//
+// PMDOE is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// PMDOE is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with PMDOE.  If not, see <http://www.gnu.org/licenses/>.
+//
+// See LICENSE.TXT file for more information.
+// -------------------------------------------------------------------
+//
+// Description : Array of WEB safe colors
+//
+//============================================================+
+?>

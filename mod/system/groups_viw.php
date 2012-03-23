13e5ab3cfa0b58bac9a33e8c4e81ce84dd682cd7
nayanajith mahendra laxaman <nml@ucsc.lk>
2012-03-23 10:18:07 +0530

    group_view compatibility fixed

--- mod/system/groups_viw.php
+++ mod/system/groups_viw.php
@@ -1,9 +1,9 @@
 <?php
 add_to_main_left("
 <h3>Manage groups</h3>
-".$GLOBALS['PREVIEW']['MAIN_LEFT']['rid']."
+".get_label('rid').get_field('rid')."
 <table>
-<tr><td>".$GLOBALS['PREVIEW']['MAIN_LEFT']['group_name']."</td><td>".$GLOBALS['PREVIEW']['MAIN_LEFT']['file_prefix']."</td></tr>
-<tr><td colspan='2'>".$GLOBALS['PREVIEW']['MAIN_LEFT']['description']."</td></tr>
+<tr><td>".get_label('group_name').get_field('group_name')."</td><td>".get_label('file_prefix').get_field('file_prefix')."</td></tr>
+<tr><td colspan='2'>".get_label('description').get_field('description')."</td></tr>
 </table>");
 ?>

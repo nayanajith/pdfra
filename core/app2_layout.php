c478df16693ebfd38c59af5d89f12c8bc98601d6
nayanajith mahendra laxaman <nml@ucsc.lk>
2012-05-22 23:26:29 +0530

    app2_layout conditional MAIN... additions

--- core/app2_layout.php
+++ core/app2_layout.php
@@ -115,11 +115,16 @@ JSON file for the menu is generated dinamically from mod/module_man/manage_modul
                   <div dojoType="dijit.layout.ContentPane" region="center" id="data_body" class="bgBottom" style="padding:5px;">
 <!--________________________start data_body area_____________________________-->
                      <div dojoType="dijit.layout.BorderContainer" style="width:100%; height:100%; padding:0px;" gutters="false" >
-                        <div dojoType="dijit.layout.ContentPane" region="top" id="MAIN_TOP" class="bgBottom" style="padding:0px;">
+                        <?php if($GLOBALS['VIEW']['MAIN_TOP'] !=""): ?>
+                        <div dojoType="dijit.layout.ContentPane" region="top" id="MAIN_TOP" class="bgBottom" style="padding:0px;height:25%">
+                           <div>
                               <?php 
                               echo $GLOBALS['VIEW']['MAIN_TOP'];
                               ?>
+                           </div>
                         </div>
+                        <?php endif; ?>
+                        <?php if($GLOBALS['VIEW']['MAIN_LEFT'] !=""): ?>
                         <div dojoType="dijit.layout.ContentPane" region="left" id="MAIN_LEFT" class="bgBottom" style="padding:0px;width:50%" liveSplitters="true">
                            <div>
                               <?php 
@@ -127,16 +132,25 @@ JSON file for the menu is generated dinamically from mod/module_man/manage_modul
                               ?>
                            </div>
                         </div>
+                        <?php endif; ?>
+                        <?php if($GLOBALS['VIEW']['MAIN_RIGHT'] !=""): ?>
                         <div dojoType="dijit.layout.ContentPane" region="right" id="MAIN_RIGHT" class="bgBottom" style="padding:0px;width:50%">
+                           <div>
                               <?php 
                               echo $GLOBALS['VIEW']['MAIN_RIGHT'];
                               ?>
+                           </div>
                         </div>
-                        <div dojoType="dijit.layout.ContentPane" region="bottom" id="MAIN_bottom" class="bgBottom" style="padding:0px;">
+                        <?php endif; ?>
+                        <?php if($GLOBALS['VIEW']['MAIN_BOTTOM'] !=""): ?>
+                        <div dojoType="dijit.layout.ContentPane" region="bottom" id="MAIN_bottom" class="bgBottom" style="padding:0px;height:75%">
+                           <div>
                               <?php 
                               echo $GLOBALS['VIEW']['MAIN_BOTTOM'];
                               ?>
+                           </div>
                         </div>
+                        <?php endif; ?>
                     </div>
 <!--_________________________end data_body area______________________________-->
                   </div>

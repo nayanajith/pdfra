47fd1cdcd59170a81c8932e043262174042feed6
nayanajith mahendra laxaman <nml@ucsc.lk>
2012-05-25 16:06:16 +0530

    common lists removed and introduced base_data

---
+++ mod/system/base_data.php
@@ -0,0 +1,13 @@
+<?php
+$GLOBALS['PAGE']=array(
+   'name'                =>'base_data',
+   'table'               =>s_t('base_data'),
+   'primary_key'         =>'rid',
+   'filter_table'        =>s_t('filter'),
+   'filter_primary_key'  =>'rid',
+);
+
+//Common control swithces included
+include A_CORE."/ctrl_common.php";
+
+?>

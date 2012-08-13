bab8210d158b53b230d3c6dc98407ffc000b069a
nayanajith mahendra laxaman <nml@ucsc.lk>
2012-08-13 18:18:06 +0530

    doc moved to home

---
+++ mod/home/user_doc.php
@@ -0,0 +1,10 @@
+<?php
+$GLOBALS['PAGE']=array(
+   'name'                =>'user_doc',
+   'table'               =>s_t('user_doc'),
+);
+
+//Common control swithces included
+include A_CORE."/ctrl_common.php";
+
+?>
---
+++ mod/home/user_doc_viw.php
@@ -0,0 +1,27 @@
+<?php
+$main_left="<form dojoType='dijit.form.Form' id='main' jsId='main' encType='multipart/form-data' method='POST' >";
+$main_left.=get_field('rid');
+$main_left.="<table>
+   <tr><td>".get_label('role_id')."</td><td>".get_field('role_id')."</td></tr>
+   <tr><td>".get_label('program_id')."</td><td>".get_field('program_id')."</td></tr>
+   <tr><td>".get_label('module_id')."</td><td>".get_field('module_id')."</td></tr>
+   <tr><td>".get_label('page_id')."</td><td>".get_field('page_id')."</td></tr>
+   <tr><td colspan='2'>".get_label('doc')."( Write in <a href='http://daringfireball.net/projects/markdown/syntax' target='_NEW'>MARKDOWN</a>)<br>".get_field('doc')." </td></tr>
+   </table></form>";
+
+add_to_main_left($main_left);
+
+$doc_arr=exec_query("SELECT * FROM ".s_t('user_doc')." WHERE rid='".get_param('rid')."'",Q_RET_ARRAY);
+$doc="";
+include_once "markdown.php";
+foreach($doc_arr as $row){
+   $doc.=Markdown($row['doc']);
+}
+add_to_main_right(
+   "<div style='border:1px solid silver;padding:10px'>Documentation preview:<div width='100%' height='100%' style='background-color:white'>$doc</div></div>"
+);
+
+
+set_layout_property('app2','MAIN_LEFT','style','width','48%');
+set_layout_property('app2','MAIN_RIGHT','style','width','50%');
+?>

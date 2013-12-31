cd05ef8475bad5d1f4532cd084fa4c08f4dc4093
Mahendra Laxaman <mahendral@millenniumit.com>
2013-12-31 12:30:43 +0530

    login page to the first item

--- mod/home/menu.php
+++ mod/home/menu.php
@@ -1,10 +1,10 @@
 <?php
 $menu_array  = array(
+   "slogin"        =>array('label'=>"Login",'visible'=>'true'),
    "news"=>"News",
    "user_doc" =>array('label'=>"Manage Documentation",'visible'=>'true'),
    "news_manage" =>array('label'=>"Manage News",'visible'=>'true'),
   // "about"=>"About",
   // "help" =>"Help",
-   "slogin"        =>array('label'=>"Login",'visible'=>'true')
 );
 ?>
